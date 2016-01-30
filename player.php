91136e84383006e59038056723c0d61c4f895011
blerou <sulik.szabolcs@ustream.tv>
2016-01-30 12:08:32 +0100

    ftw

--- player.php
+++ player.php
@@ -14,7 +14,8 @@ class Player
         $cards += $card;
       }
 
-      $playerCards = $this->playerCards($game_state);
+      $me = $this->me($game_state);
+      $playerCards = $me["hole_cards"];
     //   $encoded = http_build_query(array('cards' => $cards));
     //   $opts = array('http' =>
     //     array(
@@ -28,33 +29,32 @@ class Player
       //
     //   $response = file_get_contents('http://rainman.leanpoker.org/rank', false, $opts);
 
-        if ($game_state['community_cards']) {
-            return 1000000;
+        if ($me["stack"] > 1000) {
+            return 0;
+        // } elseif ($game_state['community_cards']) {
+        //     return 1000000;
         } else {
-            if ($playerCards[0]['rank'] == $playerCards[1]['rank']) {
+        //     if ($playerCards[0]['rank'] == $playerCards[1]['rank']) {
+        //         return 1000000;
+        //     } elseif (rand(0, 100) < 50) {
+        //         return 0;
+        //     } else {
                 return 1000000;
-            } elseif (mt_rand() < 0.5) {
-                return 0;
-            } else {
-                return 1000000;
-            }
+            // }
         }
 
     }
 
-    private function playerCards($game_state)
+    private function me($game_state)
     {
         foreach ($game_state['players'] as $player) {
-        //   if (array_key_exists("hole_cards", $player)) {
-        //       return $player["hole_cards"];
-        //   }
             if ($player["id"] == 2) {
                 file_put_contents("php://stderr", json_encode($player));
-                return $player["hole_cards"];
+                return $player;
             }
         }
         file_put_contents("php://stderr", "Nincs kartya a kezben");
-        return array();
+        return null;
     }
 
     public function showdown($game_state)
