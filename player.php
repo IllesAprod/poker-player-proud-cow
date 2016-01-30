3ffb78c7539da97813907aa6936ed4ca7aeb9974
Aprod Illes <aprodilles@Aprod-MacBook-Air.local>
2016-01-30 11:01:12 +0100

    masodik

--- player.php
+++ player.php
@@ -6,6 +6,42 @@ class Player
 
     public function betRequest($game_state)
     {
+
+      /*$card = array(
+        "rank" => "",
+        "suit" => "",
+      );
+
+      $cards = array();
+
+      foreach ($game_state['community_cards'] as $node){
+        $card['rank'] = $node['rank'];
+        $card['suit'] = $node['suit'];
+        $cards[] = $card;
+      }
+
+      foreach ($game_state['players'] as $player){
+        if ($player['id'] == 2){
+          foreach ($player['hole_cards'] as $node){
+            $card['rank'] = $node['rank'];
+            $card['suit'] = $node['suit'];
+            $cards[] = $card;
+          }
+        }
+      }
+      $encoded = http_build_query(array('cards' => $cards));
+      $opts = array('http' =>
+        array(
+          'method' => 'GET',
+          'header' => 'Content-type: application/x-www-form-urlencoded',
+          'content' => $encoded
+        )
+      );
+
+      $context = stream_context_create($opts);
+
+      $response = file_get_contents('http://rainman.leanpoker.org/rank', false, $opts);
+*/
         return 1000000;
     }
 
